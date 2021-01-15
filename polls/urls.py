d783ee7c7c5a58e080729a427955ed411355bdcc
Tomasz <tomek_kasperczyk@interia.pl>
2021-01-15 16:57:47 +0100

    dodanie widoku

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # ex /polls/
     path('', views.index, name='index'),
-    path('kot', views.index1, name='index'),
+    # ex /polls/kot
+    path('kot', views.index1, name='index1'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
